1c40b10856138a9c54f938abd3a5fc1387670a99
jroberto1802 <jroberto1802@gmail.com>
2023-09-13 15:48:33 -0300

    alterado rota do backend

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const httpClient = axios.create({
     //baseURL: 'https://b6d1-177-43-232-3.ngrok-free.app'
-    baseURL: 'http://localhost:8080'
+    baseURL: 'http://35.225.242.205:8080'
 })
 
 class ApiService {
