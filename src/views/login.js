9e1a5db1a2efeaea43c3b08359f74e2d2bb2b2d9
jroberto1802 <jroberto1802@gmail.com>
2023-09-26 15:41:05 -0300

    aplicada interface de login de acordo com o protótipo

--- src/views/login.js
+++ src/views/login.js
@@ -9,7 +9,7 @@ class Login extends React.Component {
                 <section className="background-login "> 
                     <div className="card-login">
                         <div className="card-body">
-                            <div className="text-card-login" style={{marginTop: "40px", color: "#2E384D"}}>Olá, seja bem vindo(a) à sua área de </div>
+                            <div className="text-card-login" style={{marginTop: "30px", color: "#2E384D"}}>Olá, seja bem vindo(a) à sua área de </div>
                             <h2 className="text-card-login" style={{fontWeight: 'bold', color: "#2E384D"}}>Processos Seletivos</h2>
                             <h5 className="text-card-login" style={{textAlign: "left", marginLeft: "75px", marginTop: "54px", color: "gray", textShadow: "2px 2px 4px rgba(0, 0, 0, 0.4)"}}>
                                 Acesse agora
@@ -37,7 +37,11 @@ class Login extends React.Component {
 
                             <button className="custom-login-button">
                                 ACESSAR
+                                <button className="custom-login-internal-button">
+                                    <i className="fa-solid fa-sign-in fa-lg" />
+                                </button>
                             </button>
+                            
                         </div>
                     </div>
                 </section>
